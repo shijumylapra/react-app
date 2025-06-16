2fd03f92cb03410bc5ada914b637fe288fb7bddf
shijumylapra <shijumylapra@gmail.com>
2025-06-15 21:16:00 -0600

    react-sidebar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './compnent/Sidebar'
+import './index.css'; 
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar></Sidebar>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Update paths as necessary for your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
